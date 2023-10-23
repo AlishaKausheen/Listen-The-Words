e2ac9f196d43e9784223f905712bc558c86dd46e
AlishaKausheen <alishakausheen@gmail.com>
2023-10-22 22:58:19 -0700

    tesseract some issue

--- audio.js
+++ audio.js
@@ -2,5 +2,25 @@ const video = document.querySelector(`video`);
 async function setUp() {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   video.srcObject = stream;
+  video.addEventListener(`playing`, async () => {
+    const worker = await Tesseract.createWorker();
+
+    // await worker.load();
+    // await worker.loadLanguage(`eng`);
+    //await worker.initialize(`eng`);
+
+    const canvas = document.createElement(`canvas`);
+    canvas.width = video.width;
+    canvas.height = video.height;
+    document.addEventListener(`keypress`, async (e) => {
+      if (e.code != `Enter`) return;
+      let img = canvas.getContext(`2d`).drawImage(video, 0, 0, video.width, video.height); //the image that will be used for reading
+      const {
+        data: { text },
+      } = await worker.recognize(img,`eng`);
+      console.log(text);
+      await worker.terminate();
+    });
+  });
 }
 setUp();
